cc70f72b01558126aa33dd4ee551b6b7f08178c8
David Lee <david@contest.kr>
2026-03-11 17:05:12 +0900

    Trim newline from environment variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL || "libsql://dummy",
-  authToken: process.env.TURSO_AUTH_TOKEN || "dummy",
+  url: (process.env.TURSO_DATABASE_URL || "libsql://dummy").trim(),
+  authToken: (process.env.TURSO_AUTH_TOKEN || "dummy").trim(),
 });
